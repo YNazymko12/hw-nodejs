8879ea7a8ec445212e3eb81d8090ff6eca1b7eee
YNazymko12 <yulychka12@gmail.com>
2025-01-13 13:05:30 +0100

    added reset password functionality

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -61,3 +61,21 @@ export const logoutController = async (req, res) => {
 
   res.status(204).send();
 };
+
+export const requestResetEmailController = async (req, res) => {
+  await authServices.requestResetToken(req.body.email);
+  res.status(200).json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await authServices.resetPassword(req.body);
+  res.status(200).json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
+  });
+};
